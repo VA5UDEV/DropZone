ff573c615630fb8b7b4da4b879d87da38fc240fb
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2025-07-19 14:36:06 +0530

    feat: update dashboard/home

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-// import DropboxUploader from "@/components/UploadCreateButton";
-import FileUploadForm from "@/components/FileUploadForm";
 // import FileList from "@/components/FileList";
 import { useUser } from "@clerk/nextjs";
+import UploadSheet from "@/components/UploadSheet";
 
 export default function Page() {
   const { user } = useUser();
@@ -12,9 +11,8 @@ export default function Page() {
     return null;
   }
   return (
-    <div className="flex w-full p-6">
-      {/* <DropboxUploader /> */}
-      <FileUploadForm onUploadSuccess={() => {}} />
+    <div className="flex w-full p-6 gap-6">
+      <UploadSheet />
       {/* <FileList userId={user.id} /> */}
     </div>
   );
